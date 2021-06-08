7e9c681916fa391690ddac79e49562f6a33132e7
ls1925 <103xzy@gmail.com>
2021-06-08 15:52:08 +0800

    socket char2021-6-8

--- src/main/java/com/mini/Tester0608.java
+++ src/main/java/com/mini/Tester0608.java
@@ -1,6 +1,7 @@
 package com.mini;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 public class Tester0608 {
@@ -8,7 +9,12 @@ public class Tester0608 {
         //TELNET, RFC 854
         try{
             Socket socket = new Socket("ptt.cc",23);
-                Socket socket1 = new Socket("tw.yahoo.com",88);
+//                Socket socket1 = new Socket("tw.yahoo.com",88);
+            InputStream is = socket.getInputStream();
+            for (int i = 0; i < 500; i++) {
+                int data= is.read();
+                System.out.println((char)data);
+            }
         }catch (IOException e){
             e.printStackTrace();
         }
